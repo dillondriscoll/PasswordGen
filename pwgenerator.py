f54ae8e76939a45d6c1671b0050e91458a31f48e
MathPotate <ddrisc2@live.spcollege.edu>
2017-02-16 16:37:34 -0500

    console script
    Script that generate random passwords

---
+++ pwgenerator.py
@@ -0,0 +1,20 @@
+import random
+
+def main():
+    
+    choice = int(input("Enter the length of the password(12+ characters is safest)"))
+    print(generator(choice))
+
+def generator(choice):
+    
+    letterbank = 'aeiouyAEIOUYbcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ123456789'
+    randomChars = ['!','@','#','$','%','^','&','*','(',')']
+    name = []
+    for i in range(0,choice+1):
+        name.insert(i,random.choice(letterbank))
+    randomNewChar = random.randint(0,len(name))
+    name.insert(randomNewChar,randomChars[random.randint(0,len(randomChars))])
+    newName = "".join(name)
+    return newName
+
+main()
